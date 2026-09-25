e21cb24cad5225ebd4cbeb9db181d4c1d0de4cbf
titem-012 <134424593+titem-012@users.noreply.github.com>
2026-09-25 12:14:24 +0300

    Update CorsConfig.java

--- src/main/java/com/echomenswear/config/CorsConfig.java
+++ src/main/java/com/echomenswear/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // your Next.js frontend
+                     .allowedOrigins("http://localhost:3000", "https://erp-system-uxm2.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
